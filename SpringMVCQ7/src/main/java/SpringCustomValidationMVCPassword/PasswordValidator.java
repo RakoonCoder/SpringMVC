8a49c78705286c6e098288ed3fd0ae2c9bbc2bce
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 13:04:41 +0530

    07

---
+++ SpringMVCQ7/src/main/java/SpringCustomValidationMVCPassword/PasswordValidator.java
@@ -0,0 +1,14 @@
+package SpringCustomValidationMVCPassword;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<Password,String> {  
+		  
+	    public boolean isValid(String s, ConstraintValidatorContext cvc) {  
+	          
+	        boolean result=s.contains("jtp");  
+	        return result;  
+	    }  
+  }
+
